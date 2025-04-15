b3dc7ba0727bd6c4505ee2325d42460216f01cce
Otto de Souza <ottosouza.dev@gmail.com>
2025-04-14 21:02:25 -0300

    fim

--- src/app/shared/components/select/select.component.ts
+++ src/app/shared/components/select/select.component.ts
@@ -3,13 +3,15 @@ import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { FloatLabelModule } from 'primeng/floatlabel';
 import { SelectModule } from 'primeng/select';
 import { DropdownModule } from 'primeng/dropdown';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-select',
   imports: [
     ReactiveFormsModule,
     SelectModule,
     FloatLabelModule,
-    DropdownModule
+    DropdownModule,
+    CommonModule
 
   ],
   templateUrl: './select.component.html',
@@ -21,4 +23,22 @@ export class SelectComponent {
   @Input() controlName!: string;
   @Input() label: string = 'Selecionar';
 
+  isInvalid(): boolean {
+    const control = this.formGroup?.get(this.controlName);
+    return control
+      ? control.invalid && (control.dirty || control.touched)
+      : false;
+  }
+
+  getErrorMessage(): string {
+    const control = this.formGroup.get(this.controlName);
+    if (!control || !control.errors) return '';
+  
+    if (control.errors['required']) return 'Campo obrigatório.';
+    if (control.errors['email']) return 'E-mail inválido.';
+    if (control.errors['minlength']) return `Mínimo de ${control.errors['minlength'].requiredLength} caracteres.`;
+    if (control.errors['maxlength']) return `Máximo de ${control.errors['maxlength'].requiredLength} caracteres.`;
+  
+    return 'Campo inválido.';
+  }
 }
